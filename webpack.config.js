83d0cc70889ba2a278f11a79d4ac4827aac4788e
Yuri Za <yzalvov@gmail.com>
2020-08-06 19:33:35 +0300

    Delete yarn.lock

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,6 @@ module.exports = {
     ]
   },
   externals: {
-    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+    react: 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
   }
 }
